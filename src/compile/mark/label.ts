117144a4a96b5bdf3f4ab36731fa689c0d0a815b
Will Strimling <w@ml.to>
2017-02-12 18:22:03 -0800

    revert to not using text's functions

--- src/compile/mark/label.ts
+++ src/compile/mark/label.ts
@@ -2,19 +2,40 @@ import {isFieldDef} from '../../fielddef';
 import {Config} from '../../config';
 import {isInternalData} from '../../data';
 import {FieldDef} from '../../fielddef';
-import {ANCHOR, OFFSET} from '../../channel';
-import {VgLabelTransform} from '../../vega.schema';
+import {ANCHOR, OFFSET, SIZE} from '../../channel';
+import {VgLabelTransform, VgEncodeEntry} from '../../vega.schema';
 
+import {applyConfig} from '../common';
 import {UnitModel} from '../unit';
 
+import {applyColorAndOpacity} from './common';
 import {LayoutCompiler} from './base';
-import {text} from './text';
+import {textRef} from './text';
 import * as ref from './valueref';
 
 export const label: LayoutCompiler = {
-  vgMark: text.vgMark,
-  role: text.role,
-  encodeEntry: text.encodeEntry,
+  vgMark: 'text',
+  role: undefined,
+
+  encodeEntry: (model: UnitModel) => {
+    let e: VgEncodeEntry = {};
+
+    applyConfig(e, model.config().label,
+      ['angle', 'align', 'baseline', 'dx', 'dy', 'font', 'fontWeight',
+        'fontStyle', 'radius', 'theta', 'text']);
+
+    const config = model.config();
+    const textDef = model.encoding().text;
+
+    e.fontSize = ref.midPoint(SIZE, model.encoding().size, model.scaleName(SIZE), model.scale(SIZE),
+       {value: config.text.fontSize}
+    );
+
+    e.text = textRef(textDef, config);
+    applyColorAndOpacity(e, model);
+
+    return e;
+  },
 
   transform: (model: UnitModel) => {
     const data = model.data();
